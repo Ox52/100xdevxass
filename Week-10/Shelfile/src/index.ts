b73dcf05cb73b7b7947f4866d41f7fadd69c2356
Ox52 <harsh04241@gmail.com>
2026-08-10 14:49:44 +0530

    auth

--- Week-10/Shelfile/src/index.ts
+++ Week-10/Shelfile/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+
+
+import express from "express"
+import router from "./routes/auth.routes"
+
+ const app = express()
+
+ app.use(express.json())
+
+ 
+ app.use("/api/auth" , router)
+
+
+ app.get("/", (req,res)=>{
+
+    res.json({
+
+        message:"shelflife is api is runn"  
+    })
+ })
+
+ export default app
