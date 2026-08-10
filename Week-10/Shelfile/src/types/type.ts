323d70d54b9277a4641fef35ca6312f5d9fd64fe
Ox52 <harsh04241@gmail.com>
2026-08-10 11:43:16 +0530

    types

--- Week-10/Shelfile/src/types/type.ts
+++ Week-10/Shelfile/src/types/type.ts
@@ -10,7 +10,25 @@ export interface User{
 }
 
 export interface Household {
+  name: string;
+  inviteaCode: string;
+  memebers: string[];
+  wasteScore: string;
+  admin: string;
+  createdAt: Date;
+
+}
+
+export interface Item{
 
 
-
+  householdId: string;
+  addedBy: string;
+  name: string;
+  category: |"produce" | "dairy" | "meat" | "pantry" | "frozen" | "other";
+  quantity: number;
+  expiryDate: Date;
+  status: | "fresh" | "expiring-soon" | "expired" | "used" | "wasted";
+  createdAt: Date;
+  updatedAt:Date
 }
