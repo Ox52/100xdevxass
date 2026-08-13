31cf558dcf974e00befab2ded6e30ab15e02f3db
Ox52 <harsh04241@gmail.com>
2026-08-13 23:10:46 +0530

    -

--- Week-10/Shelfile/src/controllers/auth.controller.ts
+++ Week-10/Shelfile/src/controllers/auth.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import User from "../models/User";
 import bcrypt from "bcryptjs";
 
+import { Jwt } from "jsonwebtoken";
+
 export const register = async (req: Request, res: Response) => {
   try {
     const { name, email, password } = req.body;
@@ -44,3 +46,91 @@ export const register = async (req: Request, res: Response) => {
     });
   }
 };
+
+
+export  const login = async( req:Request , res:Response) =>{
+
+    try {
+
+
+        const {emaiL , password} =req.body
+
+
+        if(!email || !password){
+
+            return res.status(400).json({
+                message: "Email and password are required",
+              });
+        }
+
+
+        const user = await User.findOne({email})
+
+        if(!user){
+            return res.status(401).json({
+                message: "Invalid email or password",
+              });
+
+            
+        }
+
+
+        const isPasswordSame = await bcrypt.compare(
+
+            password,
+            user.password
+        )
+
+
+        if(!isPasswordSame){
+
+            return res.status(401).json({
+                message: "Invalid email or password",
+              });
+        }
+
+
+        const token = jwt.sign(
+
+            {
+
+                userId: user._id,
+
+            },
+
+            process.env.JWT_SECERT!
+        )
+
+
+
+        return 
+        res.status(200).json({
+
+            message:
+            
+            "Login successful",
+
+            token,
+
+            id:  user?._id,
+            name:user?.name,
+            email:user?.emaiL
+
+
+        })
+        
+    } catch (error) {
+
+        console.error(error);
+
+        return res.status(500).json({
+          message: "Server error",
+        });
+
+      
+        
+    }
+
+
+
+}
